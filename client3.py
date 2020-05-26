11bb3b0033ee4aaa0613933f8c9470c237a395e7
simrankaur7575 <simransaini7575@gmail.com>
2020-05-26 17:18:03 +0530

    Create

--- client3.py
+++ client3.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
         prices = {}
-    for quote in quotes:
-        stock, bid_price, ask_price, price = getDataPoint(quote)
-        prices[stock] = price
-        print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-    print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
+            print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+        print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
